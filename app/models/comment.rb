1c9cb9c82504a6dca800deb838d4ac4bb7fd78ed
Andrew Mutz <andrew.mutz@gmail.com>
2015-01-18 14:41:31 -0800

    Adding model validations for Comments
    We are validating that a comment must have a message of length at
    least 1 character.  We are validating that it must be associated with a
    submission.
    
    We are creating the relation between a Comment and its parent (which is
    also a Comment).
    
    We are modifying the comments fixtures in order to have them relate to
    actual submission fixtures.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :submission
-  belongs_to :parent
+  belongs_to :parent, class_name: 'Comment'
+  has_many :replies, class_name: 'Comment', foreign_key: 'parent_id'
+
+  validates :submission, presence: true
+  validates :message, length: { minimum: 1 }
+  validates_presence_of :parent, if: 'parent_id.present?'
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,5 +1,6 @@
 class Submission < ActiveRecord::Base
   belongs_to :community
+  has_many :comments
 
   validates :community, presence: true
   validates :title, length: { minimum: 5 }, uniqueness: true
