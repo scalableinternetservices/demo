b33e09a384acaedaa0665f6a1fd9e9e3d5797eff
Andrew Mutz <andrew.mutz@gmail.com>
2015-03-04 08:49:36 -0800

    Adding database seed data
    Adding the Faker gem in order to easily generate seed data.
    
    Modified db/seeds.rb to create random communities, submissions and
    comments.  Because the function for randomness is different between
    mysql and sqlite, we need to make that dependent on the name of the
    ActiveRecord adaptor.
    
    It should be noted that this random data is not generated
    deterministically, so we should remain aware of this when using it for
    load testing.
    
    Also note that we continuously check the number of communities,
    submissions and comments that already exist in the database.  We do this
    because the seed task may be run on multiple app servers that start at
    the same time.

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'validate_url'
 
+gem 'faker'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+number_of_communities = 5
+number_of_submissions_per_community = 10
+number_of_comments_per_submission = 20
+
+if ActiveRecord::Base.connection.class.name.include?("SQLite3Adapter")
+  sql_function_random = "RANDOM()"
+else
+  sql_function_random = "RAND()"
+end
+
+number_of_communities.times do
+  if Community.count < number_of_communities
+    community = Community.create!({name: Faker::Lorem.sentence})
+    number_of_submissions_per_community.times do
+      if Submission.count < (number_of_submissions_per_community * number_of_communities)
+        submission = Submission.create!({community: community, title: Faker::Lorem.sentence, url: Faker::Internet.url})
+        number_of_comments_per_submission.times do
+          if Comment.count < (number_of_comments_per_submission * number_of_submissions_per_community * number_of_communities)
+            parent = (Random.new.rand(0..1) > 0) ? nil : submission.comments.limit(1).order(sql_function_random).take
+            Comment.create!(submission: submission, message: Faker::Lorem.paragraph , parent: parent )
+          end
+        end
+      end
+    end
+  end
+end
+
