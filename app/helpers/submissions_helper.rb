0a1a94d68ef7237cbe5c9291d1cd64b71e56df41
Andrew Mutz <andrew.mutz@gmail.com>
2015-04-19 18:01:35 -0700

    Adding caching for the submissions table
    In this example, we base our cache key on when the most recent
    submission and comment was updated.  This means that the page will be
    expired whenever a new comment or submission is created.

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,4 +1,9 @@
 module SubmissionsHelper
+
+  def cache_key_for_submission_table
+    "submission-table-#{Submission.maximum(:updated_at)}-#{Comment.maximum(:updated_at)}"
+  end
+
   def cache_key_for_submission_row(submission)
     "submission-#{submission.id}-#{submission.updated_at}-#{submission.comments.count}"
   end
