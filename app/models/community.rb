dc2730cd658c52ab1c45417de5189823c81d50ee
Bryce Boe <bbzbryce@gmail.com>
2017-12-01 16:24:01 -0800

    Cascade submission delete on community delete

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Community < ApplicationRecord
-  has_many :submissions
+  has_many :submissions, dependent: :destroy
 
   validates :name, length: { minimum: 4 }, uniqueness: true
 end
